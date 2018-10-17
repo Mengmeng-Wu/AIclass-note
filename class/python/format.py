5e8970a7898a037744d21ccf4ba9441f8691de4e
Mengmeng-Wu <mengtine4343@gmail.com>
2018-10-17 10:44:30 +0800

    Create format.py

---
+++ class/python/format.py
@@ -0,0 +1,26 @@
+## 格式化輸出Formatted Output==>兩種方法
+
+Further reading延伸閱讀 [Point Me](https://www.python-course.eu/python3_formatted_output.php)
+
+透過ASCII可去輸出此
+
+<b>[OUTPUT]</b>
+
+![](https://i.imgur.com/6PbX3QH.png)
+
+
+| 符  號|  描述| 
+| -------- |-------- | 
+| %c|Text格式化字元及其ASCII碼| 
+|%s	| 格式化字串|
+|%d	| 格式化整數|
+|%u	| 格式化無符號整型|
+|%o	| 格式化無符號八進位數|
+|%x	| 格式化無符號十六進位數|
+|%X	| 格式化無符號十六進位數（大寫）|
+|%f	| 格式化浮點數字，可指定小數點後的精度|
+|%e	|用科學計數法格式化浮點數|
+|%E	| 作用同%e，用科學計數法格式化浮點數|
+|%g	| %f和%e的簡寫|
+|%G	| %f 和 %E 的簡寫|
+|%p	| 用十六進位數格式化變數的位址|
